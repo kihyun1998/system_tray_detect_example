70a426e68c1bf63067be3d196397d3d11fdab837
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2025-03-29 14:24:11 +0900

    start system tray window detect sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,101 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:system_tray/system_tray.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(400, 300),
+    center: true,
+    title: 'Tray App Example',
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: TrayApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class TrayApp extends StatefulWidget {
+  const TrayApp({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TrayApp> createState() => _TrayAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TrayAppState extends State<TrayApp> {
+  final SystemTray _systemTray = SystemTray();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _initSystemTray();
+  }
+
+  Future<void> _initSystemTray() async {
+    const String path = 'assets/app_icon.ico';
+
+    await _systemTray.initSystemTray(
+      title: "Tray App",
+      iconPath: path,
+    );
+
+    final Menu menu = Menu();
+    await menu.buildFrom([
+      MenuItemLabel(
+        label: 'Open',
+        onClicked: (_) async {
+          await windowManager.show();
+          await windowManager.focus();
+        },
+      ),
+      MenuItemLabel(
+        label: 'Exit',
+        onClicked: (_) async {
+          await _systemTray.destroy();
+          exit(0);
+        },
+      ),
+    ]);
+
+    await _systemTray.setContextMenu(menu);
+
+    _systemTray.registerSystemTrayEventHandler((eventName) async {
+      if (eventName == kSystemTrayEventClick) {
+        await windowManager.show();
+        await windowManager.focus();
+      }
     });
+  }
+
+  Future<void> _sendToTray() async {
+    await windowManager.hide();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text('System Tray Example')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: _sendToTray,
+          child: const Text('트레이로 보내기'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
